0dc6b539f200f14951cfb2416af4da192ef8bdbf
jahnaviamaranadh <janusree1406@gmail.com>
2022-11-29 11:49:29 +0530

    arrays

---
+++ js/arrays/practise1.js
@@ -0,0 +1,8 @@
+let a=[10,20,30,40,50]
+let enames=["isha","shiva","priya","anusha","gandhi"];
+let few=[10,20.5,true,"shiv",[],{},70]
+console.log(a);
+console.log(enames);
+console.log(few);
+console.log(few[0]);
+console.log(few[7]);
---
+++ js/arrays/practise2.js
@@ -0,0 +1,33 @@
+let a=[10,20,30,40,50,60,70,80,90,100]
+let i=0;
+
+/*console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);
+console.log(a[9]);
+*/
+
+
+//to print the last term//
+/*for(let i=0;i<=0;i++)
+{
+    console.log(a[a.length-1]);
+}*/
+
+/*for(let i=0;i<=a.length-1;i++)
+{
+    console.log(a[a.length-1]);
+}*/
+ while(i<=0)
+ {
+    console.log(a[a.length-1]);
+    i++;
+ }
+

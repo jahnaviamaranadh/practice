ff569813a15dcec2a570055f49fa818a647004a5
jahnaviamaranadh <janusree1406@gmail.com>
2022-12-02 11:59:08 +0530

    functions

---
+++ js/functions/practise1.js
@@ -0,0 +1,56 @@
+function add(a,b)
+{
+ console.log(a+b);
+}
+add(22,14)
+add(10,"rahul");
+add(10)
+
+
+function cal_avg(year)
+{
+    console.log(2022-year);
+}
+cal_avg(1996)
+console.log(cal_avg(2000));
+
+
+function a()
+{
+    console.log("hello world");
+}
+a();
+
+
+function addition(a,b)
+{
+    return a+b;
+}
+let r1=addition(10,20)
+console.log(r1);
+console.log(addition(14,22));
+
+
+function wish(message)
+{
+    console.log(message);
+}
+wish("good morning.......");
+
+function wish(message)
+{
+    return "hello:     "    +message 
+}
+let me= wish("good morning all.......");
+console.log(me);
+
+
+function getdata()
+{
+    console.log("one");
+    console.log("two");
+    console.log("three");
+    return "four"
+}
+let j=getdata();
+console.log(j);

7376389cb7d49a6bb1e50fe8d9933100b8d7ad3f
jahnaviamaranadh <janusree1406@gmail.com>
2022-12-21 21:45:56 +0530

    timeout

---
+++ js/timeout/practise.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+    console.log(1)
+}, 4000);
+setTimeout(() => {
+    console.log(2)
+}, 2000);
+setTimeout(() => {
+    console.log(3)
+}, 1000);
+setTimeout(() => {
+    console.log(4)
+}, 8000)

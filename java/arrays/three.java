a71b981030598840b5f1c4ba2141b36c20e55fc8
jahnaviamaranadh <janusree1406@gmail.com>
2022-12-27 10:01:19 +0530

    arrays

---
+++ java/arrays/three.java
@@ -0,0 +1,17 @@
+class Three{
+     
+    public static void main(String[] args){
+        String[] enames= {"Rahul","Sonia","Priyanka","Modi"};
+       
+            System.out.println(enames[0]);
+            System.out.println(enames[1]);
+            System.out.println(enames[2]);
+            System.out.println(enames[3]);
+            //System.out.println(enames[4]);
+            //pritn employee
+     /*    for(String ename:enames){
+            System.out.println(ename);
+        } */
+        System.out.println(enames.length);
+    }
+}
